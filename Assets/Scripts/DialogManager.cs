bdb9475117039861e756e1bf8dbf73c9a764b5cb
Violet <56025490+a2777@users.noreply.github.com>
2025-04-16 17:31:45 -0400

    tweaked some grayboxes added more to dialogue script added a trigger for the script and added some 3d assets from unity

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -1,62 +1,37 @@
 
 using System.Collections;
 using UnityEngine;
-using TMPro;
 
-/*public class DialogManager : MonoBehaviour
+public class DialogManager : MonoBehaviour
 {
+    bool player_detection = false;
 
-    [System.Serializable]
-    public class DialogueSegment
-    {
-        public string SubjectText;
-        [TextArea]
-        public string DialogeToPrint;
-            public bool Skippable;
-        [Range(1f, 25f)]
-        public float LettersPerSecond;
-    }
-    [SerializeField] private DialogueSegment[] DialogueSegments;
-    [Space]
-    [SerializeField] private TMP_Text SubjectText;
-    [SerializeField] private TMP_Text BodyText;
-
-    private int DialogueIndex;
-    private bool PlayingDialogue;
-    private bool Skip;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+       if(player_detection && Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Dialog is triggered");
+
+        }  
     }
 
-    private IEnumerator PlayingDialogue(DialogueSegment segment)
+    private void OnTriggerEnter(Collider other)
     {
-        PlayingDialogue = true;
-        SubjectText.SetText(segment.SubjectText);
-        float delay = 1f / segment.LettersPerSecond;
-        for(int i= 0; i <segment.DialogeToPrint.Length, i++)
+        if(other.name == "Player")
         {
-            if (Skip)
-            {
-                BodyText.SetText(segment.DialogeToPrint);
-                Skip = false;
-                break;
-            }
-            string chunckToAdd = string.Empty;
-            chunckToAdd += segment.DialogueToPrint[i];
-            if(segment.DialogeToPrint[i] == " " && i< segment.DialogeToPrint.Length - 1)
-            {
-                chunckToAdd = segment.DialogeToPrint.Substring(i, 2);
-                i++;
-            }
-            BodyText.text += chunckToAdd
+            player_detection = true;
+        }
+        Debug.Log("entered trigger");
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            player_detection = false;
         }
     }
-}*/
+
+
+}
